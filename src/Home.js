251403088b1dfee56ed875d86ca173970bb4b603
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-08 20:59:57 +0530

    created custom hooks to manage data fetch

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,12 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 function Home() {
-  const [blogs, setBlogs] = useState();
-  const [isPending, setIsPending] = useState(true);
-  const [isError, setIsError] = useState();
-  useEffect(() => {
-    console.log("Use effect executed");
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        console.log(res);
-        if (!res.ok)
-          throw new error("Could not fetch data for the requested resource");
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-        setIsError();
-      })
-      .catch((e) => {
-        setIsError(e.message);
-        setIsPending(false);
-      });
-  }, []);
+  const {
+    data: blogs,
+    isError,
+    isPending,
+  } = useFetch("http://localhost:8000/blogs");
+
   return (
     <div className="home">
       {isPending && <div>Loading.....</div>}
