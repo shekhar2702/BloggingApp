568c309853b95a39ff7503884b47e8dd9a38b45f
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-08 14:17:01 +0530

    Handling render of a list.List must have unique id

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,23 @@
 import { useState } from "react";
 function Home() {
-  const [name, setName] = useState("Somya");
-  const [age, setAge] = useState(22);
-  function changeNameAndAge() {
-    console.log("Button Pressed");
-    setName("Shekhar");
-    setAge(23);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>HomePage</h2>
-      <p>
-        Hello all,{name} is {age} years old.
-      </p>
-      <button onClick={changeNameAndAge}>Press Me!</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
