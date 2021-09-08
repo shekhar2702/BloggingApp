823a39f5f58c2dea604665ddbf31be6dbc70cff9
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-08 16:52:05 +0530

    Making get request to json server data endpoint and changed the useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,11 @@
-function BlogList({ blogs, title, handleClick }) {
+function BlogList({ blogs, title }) {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
-          <p>{blog.author}</p>
-          <button onClick={() => handleClick(blog.id)}>Delete Me!</button>
+          <p>{blog.author}</p>         
         </div>
       ))}
     </div>
