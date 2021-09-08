05203bf5ce554cef7dfefe181acf38c3faf9783a
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-08 17:07:13 +0530

    Removed notes from earlier commits.

--- src/Home.js
+++ src/Home.js
@@ -9,17 +9,13 @@ function Home() {
       .then((res) => res.json())
       .then((data) => {
         setBlogs(data);
-        setIsPending(false); //setIsPending to false once data is fetched
+        setIsPending(false);
       });
   }, []);
-  //Since we use an empty dependency list we can change the state here within useEffect as useEffect is executed only on first render.
-  //But if we fill in blogs as a dependency list for useEffect then there is going to be infinite renders and executions of useEffect.Hence we use an empty
   return (
     <div className="home">
       {isPending && <div>Loading.....</div>}
-      {/* //The above code is written to show loading icon untill we get data from server.Once we get the data loading dissappears. */}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
-      {/* //Short-circuiting in above code because we only wnt to render BlogList component only when we have the data */}
     </div>
   );
 }
