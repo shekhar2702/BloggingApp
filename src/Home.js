2f9e1a0080c9b7eea3f9845a783b230428f6ee7d
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-08 17:30:21 +0530

    Handle Error:If we don't get any response from server

--- src/Home.js
+++ src/Home.js
@@ -3,18 +3,28 @@ import BlogList from "./BlogList";
 function Home() {
   const [blogs, setBlogs] = useState();
   const [isPending, setIsPending] = useState(true);
+  const [isError, setIsError] = useState();
   useEffect(() => {
     console.log("Use effect executed");
     fetch("http://localhost:8000/blogs")
-      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);//If we don't get any response from server(eg-if we are offline then we won't get any response and hence directly from here the catch block is called.)
+        return res.json();
+      })
       .then((data) => {
         setBlogs(data);
+        setIsPending(false);
+        setIsError()
+      })
+      .catch((e) => {
+        setIsError(e.message);
         setIsPending(false);
       });
   }, []);
   return (
     <div className="home">
       {isPending && <div>Loading.....</div>}
+      {isError && <div>{isError}</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
