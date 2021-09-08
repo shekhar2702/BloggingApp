8f563621f118705c25fef19909cf98acbf617e07
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-08 13:44:45 +0530

    useState basics

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,19 @@
+import { useState } from "react";
 function Home() {
-  let name = "Somya";
-  function printThis(e) {
-    console.log("Button 1 pressed", e.target);
-  }
-  function printName(name, e) {
-    console.log(name, "pressed the button 2", e.target);
+  const [name, setName] = useState("Somya");
+  const [age, setAge] = useState(22);
+  function changeNameAndAge() {
+    console.log("Button Pressed");
+    setName("Shekhar");
+    setAge(23);
   }
   return (
     <div className="home">
       <h2>HomePage</h2>
-      <button onClick={printThis}>Press Me!</button>
-      <button onClick={(e) => printName(name, e)}>Press Me!</button>
+      <p>
+        Hello all,{name} is {age} years old.
+      </p>
+      <button onClick={changeNameAndAge}>Press Me!</button>
     </div>
   );
 }
