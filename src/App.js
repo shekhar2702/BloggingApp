8cfa974c78f000a079995bef9c11487d3378e4ab
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-08 21:31:49 +0530

    Created router to for managing multiple page routes.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Home from "./Home";
 import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
 export default App;
-//testing git vs
+// - Router is placed at the root component App.
+// - We place the router around the main div which renders the entire app in this case it's the div with className = "App"
+//Switch basically matches the route and visits to a component in the ,atched route.
