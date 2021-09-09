7d0e509d71ac404e904cfe662ae855df738fe119
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-09 15:28:10 +0530

    reused the custom useFetch hook to get custom data based on blog ids.

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,10 +1,23 @@
 import { useParams } from "react-router";
-
+import useFetch from "./useFetch";
 const BlogDetail = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    isError,
+    isPending,
+  } = useFetch("http://localhost:8000/blogs/" + id);
   return (
     <div className="blog-details">
-      <h2>Blog details {id}</h2>
+      {isPending && <p>Loading.....</p>}
+      {isError && <p>{isError}</p>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by:- {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
