cb5a66fd6e80768d313868a37765fce9979338aa
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-09 14:01:44 +0530

    Removed notes from previous commits

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,16 +1,12 @@
 import { useState, useEffect } from "react";
-//The problem with this custom hook is that the fetch continues to be running in background even when the component is unmounted and other component is loaded on the screen.
-//Now when the data comes from fetch request the hook will try to change the state of the component which requested the data.
-//Now since that component is unmounted react won't be able to update the state for an unmounted component.
-//So,we need to abort the fetch process when the component gets unmounted to avoid errors.
-//Let's see hoe we can achieve that:-
+
 function useFetch(url) {
   const [data, setData] = useState();
   const [isPending, setIsPending] = useState(true);
   const [isError, setIsError] = useState();
   useEffect(() => {
     const abortController = new AbortController();
-    fetch(url, { signal: abortController.signal })//attaching the abortController to the fetch function.
+    fetch(url, { signal: abortController.signal })
       .then(function (res) {
         console.log(res);
         if (!res.ok) throw new error("Can't fetch the specified resource");
@@ -26,8 +22,8 @@ function useFetch(url) {
         setIsError(e.message);
         setIsPending(false);
       });
-    return () => abortController.abort();//This function is returned when we are aborting the fetch process.This happens if we unmount the component and another component is rendered.
-  }, [url]); //execute useEffect every time url changes.
+    return () => abortController.abort();
+  }, [url]);
   return { data, isPending, isError };
 }
 
