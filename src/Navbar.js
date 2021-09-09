0a03ce24c424db3df6d41588622a92c24cc9cbaf
shekhar2702 <shekhar.prince1234@gmail.com>
2021-09-09 12:11:48 +0530

    Added Link for single page application

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+//Link is used in place of a href and this will prevent making server requests for new routes.This is the entire point of single page applications.
 function Navbar() {
   return (
     <nav className="navbar">
       <h1>My Thoughts</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
       </div>
     </nav>
   );
